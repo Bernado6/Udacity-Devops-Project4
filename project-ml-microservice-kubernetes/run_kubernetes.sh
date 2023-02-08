27c15e0bd4451fe48e5d8164aee8c3fc3c712ac8
Ubuntu <ubuntu@ip-172-31-3-168.ec2.internal>
2023-02-08 13:55:01 +0000

    First commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath="bernardo6/mlproj:v1.1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlproj \
+	--image=bernardo6/mlproj:v1.1.0 \
+	--port=80 --labels app=mlproj
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlproj 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath="bernardo6/mlproj:v1.1.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag mlproj $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
